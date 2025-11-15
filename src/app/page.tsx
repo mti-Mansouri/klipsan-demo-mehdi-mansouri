3737786a8460400229335e5c74bc5300f5680e9d
MEHDI MANSOURI <pioneering.meti@gmail.com>
2025-11-15 01:39:53 +0100

    nav

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,7 +2,7 @@ import Image from "next/image";
 
 export default function Home() {
   return (
- <main>
+ <main className="">
   <figure className=" w-[100vw] h-[100vh] relative">
   <Image fill quality={100} src ="/Images/img-1.webp"  alt="Klipsan-header"/>
   {/* <img className="w-full h-full object-cover" src ="/Images/img-1.webp"  alt="Klipsan-header" /> */}
