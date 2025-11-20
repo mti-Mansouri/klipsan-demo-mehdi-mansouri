6baa8bbc4638e178b59169bffe9f94c6ea2360ca
MEHDI MANSOURI <pioneering.meti@gmail.com>
2025-11-20 03:00:37 +0100

    shop page

--- src/app/shop/page.tsx
+++ src/app/shop/page.tsx
@@ -1,6 +1,28 @@
+import Link from "next/link"
+import { PRODUCTS } from "@/lib/product-data"
+
 export default function ShopPage() {
 
-    return <main className="min-h-[50vh] bg-white pt-[100px]">
-        Shop page
+    return <main className="min-h-screen w-screen  bg-white pt-[120px] pb-20">
+        {/* product  */}
+        <section
+        className="px-[5%] grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 gap-10"
+        >
+            {
+                PRODUCTS.map((product,index)=>(
+                    <Link key={product.id} href={`/shop/p/${product.slug}`}>
+                        <div className="flex flex-col items-center gap-10 aspect-square" >
+                            <div className="w-full ">
+                                <img className=" object-cover w-full aspect-square" src={product.image} alt="" />
+                            </div>
+                            <div className="flex flex-col items-center">
+                                <p className="font-bebas font-bold text-[25px]">{product.name}</p>
+                                <p>{"$"+product.price} {product.subscription ? ` ${product.subscription}` : ""}</p>
+                            </div>
+                        </div>
+                    </Link>
+                ))
+            }
+        </section>
     </main>
 }
