885e01d0addc8e5217e7960163aaf3a01276a03c
MEHDI MANSOURI <pioneering.meti@gmail.com>
2025-11-20 03:17:43 +0100

    image position

--- src/app/join/page.tsx
+++ src/app/join/page.tsx
@@ -2,11 +2,11 @@ export default function JoinPage(){
     return <main className="min-h-screen w-screen bg-black">
       <header className="w-screen h-screen relative">
         <img
-          className="w-full h-full object-cover"
+          className="w-full h-full object-cover object-top"
           src="/Images/Stocksy_comp_2888472.webp"
           alt=""
         />
-        <div className=" bg-[rgba(0,0,0,0.3)_100%)] z-5 absolute w-full h-full inset-0"></div>
+        <div className=" bg-[rgba(0,0,0,0.2)_100%)] z-5 absolute w-full h-full inset-0"></div>
         <p className=" z-6 uppercase font-bebas font-bold text-[70px] absolute left-1/14 bottom-1/11 text-white">
           become a member.
         </p>
